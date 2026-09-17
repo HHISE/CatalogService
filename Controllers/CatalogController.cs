f4cdaa2a08ccc32c55808fe5a3008b86632dddd8
Hangold0078 <eaa24hgo@students.eaaa.dk>
2026-09-17 21:10:35 +0200

    Changed Get input from index to Guid

--- Controllers/CatalogController.cs
+++ Controllers/CatalogController.cs
@@ -63,20 +63,26 @@ namespace CatalogService.Controllers;
             _logger = logger;
         }
         
-        [HttpGet("productindex/{index}", Name = "GetProductAtIndex")]
-        public ActionResult<Product> Get(int index)
+        [HttpGet("product/{productId}", Name = "GetProductById")]
+        public ActionResult<Product> Get(Guid productId)
         {
             try
             {
-                _logger.LogDebug($"Getting product index");
-                var product = Catalog.GetValue(index);
+                _logger.LogDebug($"Getting product: {productId}");
+
+                var product = Catalog.FirstOrDefault(p => p.Id == productId);
+
+                if (product == null)
+                {
+                    return NotFound();
+                }
+
                 return Ok(product);
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, $"Error getting product index");
+                _logger.LogError(ex, $"Error getting product: {productId}");
                 return BadRequest();
             }
-            
         }
     }
